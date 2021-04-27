51d9a33b5dbc09fe8b29f81c470d12b365b0bf5c
xcrema <achyut.terminator@gmail.com>
2021-04-27 20:00:29 +0530

    commit

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+const socket = io();
+
+//message from server
+socket.on('message', message => {
+   console.log(message); 
+    outputMessage(message);
+
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message submit
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //get message text
+    const msg = e.target.elements.msg.value;
+    
+    socket.emit('chatMessage', msg);
+
+    //clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+//output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+
+    document.querySelector('.chat-messages').appendChild(div);
+}
